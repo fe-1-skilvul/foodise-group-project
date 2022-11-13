a8ce39f9b8f846b716599f4b55818b4937699b92
rioaldie <rioaldierwanto@gmail.com>
2022-11-13 20:28:11 +0700

    first setup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,5 @@
-import './App.css';
-import Home from './pages/home/home';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import TodoProvider from './app/TodoCtx';
-
 function App() {
-  return (
-    <Provider store={store}>
-      <TodoProvider>
-        <Home />
-      </TodoProvider>
-    </Provider>
-  );
+  return <div>app</div>;
 }
 
 export default App;
--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import todoReducer from '../pages/home/TodoSlice';
-
-export const store = configureStore({
-  reducer: {
-    todolist: todoReducer,
-  },
-});
